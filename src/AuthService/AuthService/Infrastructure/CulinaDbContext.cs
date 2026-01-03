ae7d5bfb177da72e253fa4566057b409e5fba807
ZiyaMammadov1 <zs-skayr10@outlook.com>
2026-01-03 21:09:41 +0400

    Add EF Core and PostgreSQL support; update DbContext
    Added EF Core and Npgsql dependencies to project file.
    Expanded CulinaDbContext with constructor, Users DbSet, and OnModelCreating override for entity configuration. Enables PostgreSQL integration and user entity management.

--- src/AuthService/AuthService/Infrastructure/CulinaDbContext.cs
+++ src/AuthService/AuthService/Infrastructure/CulinaDbContext.cs
@@ -1,6 +1,21 @@
-﻿namespace AuthService.Infrastructure
+﻿using AuthService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace AuthService.Infrastructure
 {
     public class CulinaDbContext : DbContext
     {
+        public CulinaDbContext(DbContextOptions<CulinaDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
